02ed5c22cff2b92ba0c1ea106d5380d195feadae
deepakkumar0077 <147796437+deepakkumar0077@users.noreply.github.com>
2025-09-16 00:21:55 +0530

    Add files via upload

---
+++ 01variable-echo.php
@@ -0,0 +1,130 @@
+<?php
+//variables---> are the containers helps us to store data temp.
+
+// $x = 10;
+// echo $x;
+
+//rules for create variable --->
+// 1) variable always start with $.
+// $x=10;
+
+
+// 2) never holds space or spacial symbols.
+// $full name = "deepak"; // (output: error)
+
+//    $fullname = "deepak";
+//    $full_name = "deepak";
+
+
+// 3) variable never start with space or any digit.
+// 4) it always start with char or underscore.
+
+// $x = 20;
+
+// $1x = 100; //(error)
+
+// $_x = 100;
+// $x1 = 1000;
+
+
+
+// 5)variable never be a keyword.
+
+// keywords are those word which are reserve by php(any language)
+// for spacial purpose.
+
+// echo, class, function, if, else, for.....
+
+// $class = 10; //(error)
+// $echo = 100; //(error)
+
+
+// 6) variable is case senstive
+
+// $Name = "deepak";
+
+// echo $Name;
+
+// echo $name; //(error)
+
+
+//-----------------------------------------------------------------
+
+// echo  |    print   |      print_r()
+
+// echo----> construct
+
+// to show output on browser-- text, number, html.....
+
+// multiple arguement can be passed
+
+
+//  echo "Hellow php!";
+
+//  echo 1000;
+
+
+// $x = "hello";
+// $y= "world";
+
+// echo $x," ",  $y;
+
+// adding two numbers----
+// $x = 1000;
+// $y = 2000;
+// // echo $x + $y;
+// $z = $x + $y;
+
+// echo "the sum of $x and $y is : $z";
+
+
+// concatenation----> joining two strings
+
+// $zx = "Hello";
+// $pq = "world";
+
+// echo $zx. " ".$pq;
+
+
+// $x = "python";
+// echo '$x is a programming language'; //(output: $x is a programming language)
+
+// \n ------> for new line 
+
+//echo  "python is a <br> \n programming language \nand php is web development";
+
+
+// \t-------> create a tag
+
+//echo "hellow\tworld";
+
+// $x = 1000;
+// $y = 2000;  
+//echo $x, $y;
+
+// echo----> multiple value show using commas.
+// echo powerfull---> do not return anything
+
+
+
+
+//------------------------------------------------------------
+
+// print -----> helps us to show single value 
+
+// $x = "hellow ";
+// $y = "okkk";
+
+// $z = print $x . $y;
+
+// print $z;
+
+//print $x,$y;  //(error) // only print single parameter
+
+
+//-------------------------------------------------------------------------
+
+// print_r() ------> function---> array, object.....
+
+// $arr = array("banana","apple","mango","orange");
+// print_r($arr);
