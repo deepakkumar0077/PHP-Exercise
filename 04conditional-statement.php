832dd29cc896fd9199cb787bd5f98c10563f82e5
deepakkumar0077 <deepak843108@gmail.com>
2025-09-17 22:44:54 +0530

    add next exercise

---
+++ 04conditional-statement.php
@@ -0,0 +1,69 @@
+<?php
+
+
+//conditional statement:------------
+
+// if , else, else if
+
+//  syntex:
+// if(condition){
+
+// }
+
+// if
+
+// $x = 77;
+// if($x < 50){
+//     echo "$x is small";
+
+// }
+
+
+
+
+
+// $x = 50;
+// if ($x < 50) {
+//     echo "$x is smaller";
+// } else {
+//     echo "$x is greater";
+// }
+
+
+
+// else if------>
+
+//  $x = 55;
+// if ($x == 50) {
+//     echo "$x is equal";
+// } else if($x<50) {
+//     echo "$x is smaller";
+// }else{
+//     echo "$x is greater";
+// }
+
+
+// echo "-----------My voating system------------>";
+// echo "<br>";
+
+// $age = 18;
+// if(!is_numeric($age)){
+//     echo "Enter a valid number";
+// }else if($age < 18){
+//     echo "not eligible";
+
+// }else if($age == 18){
+//     echo "this is your first vite";
+
+// }else{
+//     echo "eligible";
+// }
+    
+
+
+
+
+
+
+?>
+
