d2f104b1636d0ae2fd2ab8fa5a039a8f2baa458b
deepakkumar0077 <deepak843108@gmail.com>
2025-09-18 23:49:46 +0530

    add next exercise

---
+++ 05loop.php
@@ -0,0 +1,78 @@
+<?php
+
+// Loop
+
+// 1) while loop
+// 2) Do/while loop
+// 3) for loop
+// 4)forEach
+
+
+
+
+// while loop ------->
+// The while loop executes a block of code repeatedly until the condition is FALSE.
+// Once the condition gets FALSE, it exits from the body of loop.
+
+// $a = 1;
+// while($a<=10){
+//     echo $a." Hello world!<br>";
+//     $a++;
+// }
+
+
+// $a = 10;
+// while($a>=1){
+//     echo $a." hello world<br>";
+//     $a--;
+// }
+
+
+// $a = 10;
+// while($a>=1){
+//     echo $a." hello world<br>";
+//     $a=$a-2; // decrement of 2
+// }
+
+
+// $a = 1;
+// while($a<=100){
+//     echo $a." hello world<br>";
+//     $a=$a+3; // increment of 3
+// }
+
+
+
+
+// do/while loop------->
+// It is guaranteed to execute the code at least one time always
+// because the condition is checked after executing the code.
+
+
+// $a =1;
+// do{
+//     echo $a." hello world <br>";
+//     $a++;
+
+// }while($a<=10)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
